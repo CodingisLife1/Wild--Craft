6edeb2736bfb67bce67f7d0c9f39280648678e5c
Vedenkin_Ivan <63848929+CodingisLife1@users.noreply.github.com>
2024-05-30 11:43:17 +0300

    Правки

---
+++ WildCraft/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public UIManager uIManager;
+    public Player player;
+    public LootZone lootZone;
+    public Transform cam_transform;
+    public QuestManager questManager;
+    public SpawnLootZones spawnLootZones;
+    public int animalIndex;
+    public Vector3 lastPos;
+    public EnemySpawnSystem spawn;
+    public CinemachineFreeLook cm;
+    public GameObject additionalQuests;
+    public GameObject cursor;
+    public Transform startPoint;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+}
